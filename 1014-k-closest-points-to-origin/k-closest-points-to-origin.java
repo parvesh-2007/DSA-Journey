cd44e1c3ca791f8d174da524f088bb9dbe1491f4
parvesh-2007 <pk8592423@gmail.com>
2026-08-30 19:07:39 +0530

    Time: 31 ms (50.24%) | Memory: 58.9 MB (20.64%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,38 +1,34 @@
 class Solution {
-    static class Point{
+    public class Point{
         int x;
         int y;
         int distSq;
-        public Point(int x, int y, int distSq) {
-        this.x = x;
-        this.y = y;
-        this.distSq = distSq;
+        public Point(int x, int y, int distSq){
+            this.x = x ; 
+            this.y = y;
+            this.distSq= distSq;
         }
     }
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Point> pq = new PriorityQueue<>((a,b)->b.distSq - a.distSq);
-        
-        for(int i = 0; i<points.length; i++){
+    PriorityQueue<Point>pq = new PriorityQueue<>((a,b)-> b.distSq-a.distSq);
+     for(int i = 0; i< points.length; i++){
         int x = points[i][0];
         int y = points[i][1];
-
-        int distSq = x*x +y*y;
-        pq.add(new Point(x, y, distSq));
-        if(pq.size()>k){
+        int distSq = x * x + y * y;
+        pq.add(new Point( x, y, distSq));
+        if(pq.size() > k){
             pq.remove();
         }
-        }
-        int[][] ans = new int[k][2];
-        for(int i = 0; i<k; i++){
-            Point point = pq.remove();
-            ans[i][0] = point.x;
-            ans[i][1] = point.y;
-        }
-        return ans;
 
+
+     }
+     int[][] ans = new int[k][2];
+     for(int i = 0; i<k; i++){
+       Point p = pq.remove();
+       ans[i][0] = p.x;
+       ans[i][1] = p.y;
+     }
+     return ans;
         
     }
 }
-
-
-
